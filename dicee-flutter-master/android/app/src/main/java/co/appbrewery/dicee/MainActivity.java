e7d2c0ae40b2872792af6d85d08ff0b7499c88b0
hitaarthh <hitarth.rohra@gmail.com>
2022-03-13 15:18:28 +0530

    Commit 6

--- dicee-flutter-master/android/app/src/main/java/co/appbrewery/dicee/MainActivity.java
+++ dicee-flutter-master/android/app/src/main/java/co/appbrewery/dicee/MainActivity.java
@@ -1,13 +1,16 @@
 package co.appbrewery.dicee;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
-import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+   public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+           GeneratedPluginRegistrant.registerWith(flutterEngine);
+            new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                           .setMethodCallHandler(
+                                (call, result) -> {
+                                      // Your existing code
+                                      }
+                            );
+        }
 }
